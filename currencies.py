9ba66897716e2341ef499b2881934a26b238dc34
Sergey Radyukhin <22dollars@mail.ru>
2022-06-04 15:39:43 +0300

    fixed currencies.py

--- currencies.py
+++ currencies.py
@@ -1,9 +1,26 @@
 import requests
 import settings
 
-# Запрашиваем валютные пары: RUBUSD, RUBEUR, RUBGBP, RUBJPY, RUBTRY
-# Документация https://apilayer.com/marketplace/fixer-api?preview=true#
 def get_currencies(symbols, base):
+    """
+    Фукнция должна вернуть соотношение валютных пар RUBUSD, RUBEUR, RUBGBP, RUBJPY, RUBTRY
+    в формате:
+    {
+        "base": "RUB",
+        "date": "2022-06-04",
+        "rates": {
+            "EUR": 0.014723,
+            "GBP": 0.012636,
+            "JPY": 2.064721,
+            "TRY": 0.259868,
+            "USD": 0.015783
+        },
+        "success": true,
+        "timestamp": 1654345755
+        }
+    Документация к API https://apilayer.com/marketplace/fixer-api?preview=true#
+
+    """
     url = settings.currencies_URL
     params = {
         'symbols': symbols,
@@ -17,12 +34,10 @@ def get_currencies(symbols, base):
     try:
         result = requests.get(url, params=params, headers=headers, data = payload)
         result.raise_for_status()
+        #status_code = response.status_code
         currencies = result.json()
-        if currencies['success'] == True:
-            try:
-                return currencies['rates']
-            except(IndexError, TypeError):
-                return False
+        if currencies.get('success') is True:
+            return currencies.get('rates')
     except(requests.RequestException, ValueError):
         print('Network error')
         return False
