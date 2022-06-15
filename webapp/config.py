4513ec7e54cf364f4f73ea58f3d8278dfa7ba629
sraduhin <103758131+sraduhin@users.noreply.github.com>
2022-06-15 15:51:14 +0300

    Delete config.py

--- webapp/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-basedir = os.path.dirname(__file__)
-
-CURRENCIES_API_KEY = "PHYlozjcZ5z6aDwXStIkjSYq40KWfRDi"
-TINKOFF_API_KEY = "t.KuJkD0UfDsdvKMYCE7iAxDFEBp9Wn46EcKUK3CVGFqnsT_EqHKxuqByOWzT-zCYBAKKJz0VYOJ1LX5fMHnIO0g" # IIS
-CURRENCIES_URL = "https://api.apilayer.com/fixer/latest"
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
