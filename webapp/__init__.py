1ad1f5c403e1de4282a69886119b87a7c9c1ef40
Sergey Radyukhin <22dollars@mail.ru>
2022-06-23 18:18:28 +0300

    add query and fix templates

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 
 from webapp.model import db, Position
+from queries import get_position_row
+import json
 
 
 def create_app():
@@ -20,6 +22,9 @@ def create_app():
         # Сохраним запросы API
         # currencies = get_currencies('%2C%20'.join(tikers), base_currency)
         currencies = {'USD': 0.015873, 'EUR': 0.014775, 'GBP': 0.012637, 'JPY': 2.071604, 'TRY': 0.26239}
-        securities = Position.query.all()
-        return render_template('index.html', page_title=title, currencies=currencies, tikers=tikers, base_currency=base_currency, securities=securities)
+        securities = get_position_row('2000377867')
+        historycal_portfolio_data = {'Monday': 1000, "Tuesday": 900, "Wednesday": 1230, "Thursday": 1400, "Friday": 1800, "Saturday": 1700, "Sunday": 1500}
+        labels = list(historycal_portfolio_data.keys())
+        values = list(historycal_portfolio_data.values())
+        return render_template('index.html', page_title=title, currencies=currencies, tikers=tikers, base_currency=base_currency, securities=securities, labels=labels, values=values)
     return app
