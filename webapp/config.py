5dc5a5a77e35a62a4ca3328945876fb694eccb32
sraduhin <103758131+sraduhin@users.noreply.github.com>
2022-06-27 10:16:19 +0300

    Delete config.py

--- webapp/config.py
+++
@@ -1,3 +0,0 @@
-currencies_API_KEY = "PHYlozjcZ5z6aDwXStIkjSYq40KWfRDi"
-TINKOFF_API_KEY = "t.KuJkD0UfDsdvKMYCE7iAxDFEBp9Wn46EcKUK3CVGFqnsT_EqHKxuqByOWzT-zCYBAKKJz0VYOJ1LX5fMHnIO0g" # IIS
-currencies_URL = "https://api.apilayer.com/fixer/latest"
