6413c6fbf4433033e38f4524f9f5d96cbfb12ff6
Sergey Radyukhin <22dollars@mail.ru>
2022-07-01 09:53:11 +0300

    finish task

--- create_admin.py
+++ create_admin.py
@@ -6,23 +6,27 @@ from webapp.user.models import db, User
 
 app = create_app()
 
-with app.app_context():
-    username = input('Name: ')
+def create_admin():
+    with app.app_context():
+        username = input('Name: ')
 
-    if User.query.filter(User.username == username).count():
-        print('This name is already taken')
-        sys.exit(0)
+        if User.query.filter(User.username == username).count():
+            print('This name is already taken')
+            sys.exit(0)
 
-    password = getpass('Password: ')
-    password2 = getpass('Repeat password: ')
-    
-    if not password == password2:
-        print('Pass doesnt match')
-        sys.exit(0)
+        password = getpass('Password: ')
+        password2 = getpass('Repeat password: ')
+        
+        if not password == password2:
+            print('Pass doesnt match')
+            sys.exit(0)
 
-    new_user = User(username=username, role='admin')
-    new_user.set_password(password)
+        new_user = User(username=username, role='admin')
+        new_user.set_password(password)
 
-    db.session.add(new_user)
-    db.session.commit()
-    print('User with id {} added'.format(new_user.id))
+        db.session.add(new_user)
+        db.session.commit()
+        print('User with id {} added'.format(new_user.id))
+
+if __name__ == '__main__':
+    create_admin()
